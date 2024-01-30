265c355cf2164488be7f28ad07932a9321c0bea4
Soniya Rayabagi <soniyaraibagi@gmail.com>
2024-01-30 17:33:43 +0530

    deploying-single-web-server

--- terraform/chatper-2/main.tf
+++ terraform/chatper-2/main.tf
@@ -1,11 +1,21 @@
-provider "aws" { 
-    region = "us-east-2"
+provider "aws" {
+  region = "us-east-2"
 }
-resource "aws_instance" "example" {
-ami = "ami-0fb653ca2d3203ac1" 
-instance_type = "t2.micro"
 
-tags={
-Name = "terraform-example"
-}
+resource "aws_instance" "example" {
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-03c288ea670e49d8b"]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    user_data_replace_on_change = true
+
+    tags={
+        Name = "terraform-example"
+    }
 }
